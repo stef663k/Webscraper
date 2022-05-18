7a5e6149ea04fb07caab8632bb079568f666ac1d
stef663k <stef663k@elev.techcollege.dk>
2022-05-18 10:32:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+namespace Webscraper;
+
+class program
+{
+    public static StreamWriter writers { get; private set; }
+    public static CsvWriter csv { get; private set; }
+
+    static void Main(string[] args)
+    {
+        HtmlWeb web = new HtmlWeb();
+
+        HtmlDocument doc = web.Load("https://en.wikipedia.org/wiki/Denmark");
+
+        var HeaderNames = doc.DocumentNode.SelectNodes("//span[@class='toctext']");
+        
+
+        var titles = new List<Row>();
+
+        foreach(var item in HeaderNames)
+        {
+            titles.Add(new Row { Title = item.InnerText});
+        }
+        
+        using ( writers = new StreamWriter("..\\Wiki.csv"))
+
+        using(csv = new CsvWriter(writers, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(titles);
+        }
+    }
+
+    public class Row
+    {
+        public string Title { get; set; }
+    }
+}
+
